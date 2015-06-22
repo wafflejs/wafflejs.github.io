a793d44efe1d229f073833544879adca30f62947
Visnu Pitiyanuvath <visnu@opendoor.com>
2015-06-21 17:28:44 -0700

    styling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import normalize from 'normalize.css';
+import css from './index.css';
+
 import marked from 'marked';
 
 marked.setOptions({ smartypants: true });
 
 const pre = document.querySelector('pre');
-pre.outerHTML = marked(pre.innerHTML);
+pre.outerHTML = `<div class="container">${marked(pre.innerHTML)}</div>`;
