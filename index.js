aad3c9a2f14f48821b9c77360675f756d7f27ffd
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-06-25 23:45:05 -0700

    maybe more understandable head tags

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import normalize from 'normalize.css';
 import css from './index.css';
 
-import marked from 'marked';
+const head = document.createElement('div');
+head.innerHTML = `
+  <link href="http://fonts.googleapis.com/css?family=Lato:400,700|Alegreya:700" rel="stylesheet" type="text/css" />
+  <meta name="viewport" content="initial-scale=1" />
+  <title>waffle.js</title>
+`;
+while (head.childNodes.length)
+  document.head.appendChild(head.childNodes[0]);
 
+import marked from 'marked';
 marked.setOptions({ smartypants: true });
 
 const pre = document.querySelector('pre');
 pre.outerHTML = `<div>${marked(pre.innerHTML)}</div>`;
-
-document.title = 'waffle.js';
-
-const fonts = document.createElement('link');
-fonts.href = 'http://fonts.googleapis.com/css?family=Lato:400,700|Alegreya:700';
-fonts.rel = 'stylesheet';
-fonts.type = 'text/css';
-document.head.appendChild(fonts);
-
-const viewport = document.createElement('meta');
-viewport.name = 'viewport';
-viewport.content = 'initial-scale=1';
-document.head.appendChild(viewport);
