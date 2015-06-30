78f56e45b96660a6eac2de06506bc88e66dd5e7a
Billy Roh <billy.roh@gmail.com>
2015-06-30 11:02:58 -0700

    requested changes

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ head.innerHTML = `
   <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
   <meta name="viewport" content="initial-scale=1" />
   <title>WaffleJS</title>
-  <link rel="icon" type="image/png" href="favicon.png" sizes="16x16">
-  <link rel="icon" type="image/png" href="favicon@2x.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="favicon.png">
 `;
 while (head.childNodes.length)
   document.head.appendChild(head.childNodes[0]);
