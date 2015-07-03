166aff475b16fc12994a00b78168d57dbaa206b8
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-07-03 16:24:02 -0700

    assume roboto mono for consistency

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import css from './index.css';
 
 const head = document.createElement('div');
 head.innerHTML = `
-  <link href="http://fonts.googleapis.com/css?family=PT+Sans|Roboto+Mono:400,700" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=PT+Sans|Roboto+Mono:700" rel="stylesheet" type="text/css">
   <meta name="viewport" content="initial-scale=1" />
   <title>WaffleJS</title>
 `;
