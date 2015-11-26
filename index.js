488bedcfbaa5973c4ee1bdb5ebde817c9e1bbdf9
Visnu Pitiyanuvath <visnup@gmail.com>
2015-11-26 15:32:12 -0800

    wip: react

--- index.js
+++ index.js
@@ -1,20 +1,81 @@
-import heap from './heap';
-import css from './index.css';
+//import heap from './heap'
+import css from './index.css'
 
-const head = document.createElement('div');
-head.innerHTML = `
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400|PT+Sans:400,700" rel="stylesheet" type="text/css">
-  <meta name="viewport" content="initial-scale=1,user-scalable=no" />
-`;
-while (head.childNodes.length)
-  document.head.appendChild(head.childNodes[0]);
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, IndexRoute, Route, Link } from 'react-router'
+import Markdown from 'matthewmueller-react-remarkable'
+import Sponsorship from './sponsorship'
 
-import React from 'react';
-import { render } from 'react-dom';
-import Markdown from './markdown';
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <div className="container">
+            <div className="row bottom-sm">
+              <div className="col-xs-12 col-sm-6">
+                <Markdown>{`
+                  #### WaffleJS
 
-const src = document.querySelector('noscript');
+                  A night of code, waffles, and karaoke.
+                  ======================================
+
+                  ### All proceeds go to [Girls Who Code][1].
+
+                  [1]: girlswhocode
+                `}</Markdown>
+              </div>
+              <div className="col-xs-12 col-sm-6">
+                <Markdown>{`
+                  ##### Date & Time
+                  ### Wednesday, December 2 at 7 PM
+
+                  ##### Location
+                  ### [SOMA StrEat Food][1] (in the [barn][2])
+
+                  [1]: foo.com
+                  [2]: /map
+                `}</Markdown>
+              </div>
+            </div>
+          </div>
+        </header>
+        <section id="about">
+          <div className="container">
+            <Markdown>{`
+              About
+              -----
+
+              WaffleJS is a meetup about web technology with a side of waffles
+              and karaoke. We meet on the first Wednesday of every month.
+
+              All participants are expected to follow the [JSConf Code of
+              Conduct][1].
+
+              [1]: jsconf
+            `}</Markdown>
+          </div>
+        </section>
+        <section id="schedule">
+          <div className="container">
+            <h2>Schedule</h2>
+          </div>
+        </section>
+        <footer>
+        </footer>
+      </div>
+    )
+  }
+}
+
 render(
-  <Markdown src={src.innerText || src.innerHTML} className={src.className}/>,
+  <Router>
+    <Route path="/">
+      <IndexRoute component={Index} />
+      <Route path="sponsorship" component={Sponsorship} />
+      <Route path="speakers" component={Sponsorship} />
+    </Route>
+  </Router>,
   document.body
-);
+)
